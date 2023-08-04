9bbb2e1bd0db4db70503481a5a32445658898fb3
deba793 <debasisrout793@gmail.com>
2023-08-04 17:51:18 +0530

    MainProject-4

---
+++ TeacherwithSubjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject_4
+{
+    public class TeacherwithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherwithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+}
